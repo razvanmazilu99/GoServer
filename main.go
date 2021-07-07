d478844c60184eea593d82e4b6dd0ccba9c67e11
razvanmazilu99 <mazilu.razvan.moisil@gmail.com>
2021-07-07 11:55:30 +0300

    Get and Post

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", rest.Welcome)
-	chi := chi.NewRouter()
+	http.HandleFunc("/health", rest.Welcome)
+	router := chi.NewRouter()
+	router.Route("/v1", func(r chi.Router) {
+		r.Get("/person", rest.Welcome)
+		r.Post("/person", rest.Welcome)
+	})
 	http.ListenAndServe(":8080", nil)
 }
